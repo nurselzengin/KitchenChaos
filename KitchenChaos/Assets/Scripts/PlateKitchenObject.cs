b13cf211190385900ba6a2a823b30d022752d8f1
nurselzengin <125567986+nurselzengin@users.noreply.github.com>
2023-07-21 12:21:22 +0300

    Plate Bug fixed

--- KitchenChaos/Assets/Scripts/PlateKitchenObject.cs
+++ KitchenChaos/Assets/Scripts/PlateKitchenObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlateKitchenObject : KitchenObject
 {
@@ -8,22 +9,18 @@ public class PlateKitchenObject : KitchenObject
     [SerializeField] private List<KitchenObjectsSO> validKitchenObjectsSOList;
 
     private List<KitchenObjectsSO> kitchenObjectsSOList;
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectsSO kitchenObjectsSO;
+    }
 
     private void Awake()
     {
         kitchenObjectsSOList = new List<KitchenObjectsSO>();
     }
-    void Start()
-    {
-        
-    }
-
-   
-    void Update()
-    {
-        
-    }
-
+    
     public bool TryAddIngredient(KitchenObjectsSO kitchenObjectsSO)
     {
         if(!validKitchenObjectsSOList.Contains(kitchenObjectsSO))
